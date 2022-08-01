71b707275e68535492b52a4c0299fc2557ba1541
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2022-08-01 15:41:49 +0600

    first update

---
+++ index.js
@@ -0,0 +1,15 @@
+var age = [2, 4, 5, 7, 9]
+
+//get element value by index
+console.log(age[2]);
+
+//get element value by index
+age[1] = 15;
+age[2] = 5;
+
+console.log(age);
+
+//find index of an element
+var PositionIndex = age.indexOf(9);
+var PositionIndex = age.indexOf(17);
+console.log(PositionIndex)
